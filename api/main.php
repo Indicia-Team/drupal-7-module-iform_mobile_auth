ec9748f3d7b9e2af129d19e000878175685b4891
kazlauskis <karolis@kazlauskis.com>
2017-01-20 12:51:26 +0000

    init 2

--- api/main.php
+++ api/main.php
@@ -5,3 +5,25 @@
  * Date: 20/01/2017
  * Time: 10:36
  */
+
+require "samples.php";
+
+define("API_BASE", "api");
+define("API_VERSION", "v0.1");
+
+/**
+ * Implements hook_menu().
+ */
+function api_menu() {
+  $items = array();
+  $api_path = API_BASE."/".API_VERSION;
+
+  // Mobile based record submission.
+  $items["$api_path/samples"] = array(
+    'title'             => 'Samples',
+    'page callback'     => 'iform_mobile_auth_samples',
+    'access callback'   => TRUE,
+  );
+
+  return $items;
+}
